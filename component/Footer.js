543acf573a3a6c1bc1df922269c5c7972664dc95
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-15 15:46:59 +0000

    added Foot component

---
+++ component/Footer.js
@@ -0,0 +1,26 @@
+import styles from '../styles/Home.module.css'
+import todoStyle from '../styles/Todo.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Col, Button, Container} from 'reactstrap'
+
+function Foot() {
+    return ( 
+        <Container>
+            <Row className={todoStyle.box}>
+                <Col xs="3">
+                    items left
+                </Col>
+                <Col xs="6">
+                    <Button color="secondary" outline>All</Button>
+                    <Button color="secondary" outline>Active</Button>
+                    <Button color="secondary" outline>Complete</Button>
+                </Col>
+                <Col>
+                    <Button color="secondary" outline>Clear All</Button>
+                </Col>
+            </Row>
+        </Container>
+     );
+}
+
+export default Foot;
