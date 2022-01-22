09a79db77ad8b5cca486fdc362931c9975462553
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-22 23:42:04 +0000

    h12 error fix attempt

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+}
+
+module.exports = {
   env: {
     NEXT_PUBLIC_MYSQL_HOST='eu-cdbr-west-02.cleardb.net',
     NEXT_PUBLIC_MYSQL_DATABASE='heroku_faa22905fef5e7b',
