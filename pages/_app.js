91afc56ae020e3c678cee877730435076b9b0292
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-25 09:06:51 +0000

    implemented signin with google provider neaxtauth attempting to connect to db

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,23 @@ import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from '../lib/reducer'
 import thunk from 'redux-thunk'
+import {SessionProvider} from 'next-auth/react'
 
 export const store = createStore(reducer, applyMiddleware(thunk))
 
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <Layout>
       <Provider store={store}>
-        <Component {...pageProps}/>
+        <SessionProvider session={session} refetchInterval={5 * 60}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </Provider>
     </Layout>
   )
 }
 
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import todoStyle from '../styles/Todo.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'reactstrap'
+import {Container, Row, Col, Button} from 'reactstrap'
 import Record from '../component/Record';
 import Foot from '../component/Footer.js'
 import {connect} from 'react-redux'
 import * as actionCreators from '../lib/actions.js'
+import {signIn, useSession, signOut} from 'next-auth/react'
 
 function Home(props) {
+  const {data: session} = useSession()
   const [inp, setInp] = useState('')
   let stat = props.todos
   let statCp = props.todosCp
@@ -16,27 +18,37 @@ function Home(props) {
   useEffect(()=>{
     props.loadData()
   }, [])
-  return (
-    <Container className={styles.grid}>
-        <Row>
-          <Col>
-            <form onSubmit={(e)=>{e.preventDefault(); props.insertData(inp); setInp('')}}>
-              <input type="text" className={todoStyle.input} onChange={(e)=>{setInp(e.target.value)}} value={inp} placeholder="What need to be done?"/>
-            </form>
-          </Col>
-        </Row>
-        <Row>
-          <div>
-              {stat.map((el)=>(
-                <Record key={el.id} record={el} rd={props}/>
-            ))}
-          </div>
-        </Row>
-        <Row>
-          {(stat.length > 0) || (statCp.length > 0) ? <Foot left={stat.length} rd={props} clearInp={setInp} /> : null}
-        </Row>
-    </Container>
-  )
+
+  if (session) {
+    return (
+      <Container className={styles.grid}>
+        {console.log(session.user.email.concat('_database'))}
+          <Row>
+            <Button color="primary" onClick={()=>signOut()}>Sign Out</Button>
+          </Row>
+          <Row>
+            <Col>
+              <form onSubmit={(e)=>{e.preventDefault(); props.insertData(inp); setInp('')}}>
+                <input type="text" className={todoStyle.input} onChange={(e)=>{setInp(e.target.value)}} value={inp} placeholder="What need to be done?"/>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <div>
+                {stat.map((el)=>(
+                  <Record key={el.id} record={el} rd={props}/>
+              ))}
+            </div>
+          </Row>
+          <Row>
+            {(stat.length > 0) || (statCp.length > 0) ? <Foot left={stat.length} rd={props} clearInp={setInp} /> : null}
+          </Row>
+          
+      </Container>
+    )
+  } else {
+    return <Button color="primary" onClick={()=>signIn()}>Sign In</Button>
+  }
 }
 
 function mapStateToProps(state) {
