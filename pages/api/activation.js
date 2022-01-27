1d8fd1fc925a7661113e092a10b4f8031d66253d
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-27 22:09:36 +0000

    func name in activate.js

--- pages/api/activation.js
+++ pages/api/activation.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { sql_query } from '../../lib/db.js'
 
-export default async function(req, res) {
+export default async function activate(req, res) {
     const {id, isActive, email} = req.body
     let dbName = email.split('@')
     dbName = dbName[0]
