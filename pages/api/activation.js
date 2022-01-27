f4faea45794d1001fd58b8b1075083f61d95b922
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-27 19:49:10 +0000

    activation.js update

--- pages/api/activation.js
+++ pages/api/activation.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { sql_query } from '../../lib/db'
+import { sql_query } from '../../lib/db.js'
 
 export default async function(req, res) {
     const {id, isActive, email} = req.body
