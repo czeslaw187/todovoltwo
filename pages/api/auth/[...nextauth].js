beef59bc6c3b927b7a564a4b8acb802a21e32813
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-28 02:00:41 +0000

    secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,5 +9,6 @@ export default NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
-    ]
+    ],
+    secret: process.env.SECRET
 })
