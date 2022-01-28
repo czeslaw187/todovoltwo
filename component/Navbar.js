680dff3bb3fd3f06ae41007dfc31dea3685e9325
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-28 10:36:28 +0000

    navbar styling

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,28 +1,41 @@
 import navStyle from '../styles/Nav.module.css'
 import Link from 'next/link'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink, Nav} from 'reactstrap'
 import {useSession, signOut, signIn} from 'next-auth/react'
 // 07914253609 
 function Navbar() {
     const {data: session} = useSession()
     return ( 
-        <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-light bg-light">
-            <div className='container-fluid'> 
-                <a className={navStyle.brand} href="#">ToDo</a>               
-                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#myNavbar'>
-                    <span className='navbar-toggler-icon' ></span>
-                </button>
-                <div className='collapse navbar-collapse justify-content-end' id='myNavbar'>
+        <Navbar color='ligh' expand='md' light>
+            <NavbarBrand  className={navStyle.brand} href='#'>
+                ToDo
+            </NavbarBrand>
+            <NavbarToggler onClick={function noRefCheck(){}}/>
+            <Collapse navbar/>
+            <Nav className='ml-auto' navbar>
+                <NavItem>
+                    {session ? <NavLink href="#" onClick={()=>signOut()}>Sign out</NavLink> : <NavLink href="#" onClick={()=>signIn()}>Sign in</NavLink>}
+                </NavItem>
+            </Nav>
+        </Navbar>
+
+        // <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-light bg-light">
+        //     <div className='container-fluid'> 
+        //         <a className={navStyle.brand} href="#">ToDo</a>               
+        //         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#myNavbar'>
+        //             <span className='navbar-toggler-icon' ></span>
+        //         </button>
+        //         <div className='collapse navbar-collapse justify-content-end' id='myNavbar'>
                     
-                    <ul className='navbar-nav ml-auto'>
-                        <li className='nav-item'>
-                            {session ? <a className='nav-link' href="#" onClick={()=>signOut()}>Sign out</a> : <a className='nav-link' href="#" onClick={()=>signIn()}>Sign in</a>}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        //             <ul className='navbar-nav ml-auto'>
+        //                 <li className='nav-item'>
+        //                     {session ? <a className='nav-link' href="#" onClick={()=>signOut()}>Sign out</a> : <a className='nav-link' href="#" onClick={()=>signIn()}>Sign in</a>}
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </nav>
     );
 }
 
