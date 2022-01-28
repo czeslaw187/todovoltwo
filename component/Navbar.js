695e6696ea3a87d570499c5c764ffbee943651e5
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-28 02:32:38 +0000

    nav toggler button fix

--- component/Navbar.js
+++ component/Navbar.js
@@ -8,12 +8,13 @@ function Navbar() {
     const {data: session} = useSession()
     return ( 
         <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-light bg-light">
-            <div className='container-fluid'>                
+            <div className='container-fluid'> 
+                <a className={navStyle.brand} href="#">ToDo</a>               
                 <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#myNavbar'>
                     <span className='navbar-toggler-icon' ></span>
                 </button>
                 <div className='collapse navbar-collapse justify-content-between' id='myNavbar'>
-                    <a className={navStyle.brand} href="#">ToDo</a>
+                    
                     <ul className='navbar-nav ml-auto'>
                         <li className='nav-item'>
                             {session ? <a className='nav-link' href="#" onClick={()=>signOut()}>Sign out</a> : <a className='nav-link' href="#" onClick={()=>signIn()}>Sign in</a>}
