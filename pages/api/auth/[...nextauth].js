3e92c21d50d7e6ad5c4b65fa884e5867c295d934
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-01-28 02:09:48 +0000

    1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,6 @@ import GoogleProvider from "next-auth/providers/google"
 
 
 export default NextAuth({
-    site: process.env.NEXTAUTH_URL,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
