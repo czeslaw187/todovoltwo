4c8aeae4dc6396b7c1c64775818c9dc3484eb4d6
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-02-05 15:53:57 +0000

    subscribe page style and logics

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,8 +1,11 @@
 import {useSession, signOut, signIn} from 'next-auth/react'
 import Link from 'next/link'
+import {useRouter} from 'next/router'
+
 
 function NavBar() {
     const {data: session} = useSession()
+    const router = useRouter()
     return ( 
         <div className='flex sticky w-full h-16 bg-gradient-to-tr from-blue-100 to-blue-500 rounded-t-xl p-4'>
             <Link href='/'>
@@ -10,11 +13,18 @@ function NavBar() {
             </Link>  
             <ul className='w-full flex justify-end text-gray-100 text-md'>
                 <li className='list-none'>
-                    {session ? <a className='text-xl font-lg hover:text-sky-300' href="#" onClick={()=>signOut()}>Sign out</a> : <a className='text-xl font-lg hover:text-sky-300' href='#' onClick={()=>signIn()}>Sign in</a>}
+                    {session ? <a className='text-xl font-lg hover:text-sky-300 cursor-pointer' onClick={()=>handleSignOut()}>Sign out</a> : <a className='text-xl font-lg hover:text-sky-300' href='#' onClick={()=>signIn()}>Sign in</a>}
                 </li>
             </ul>
         </div>
     );
+}
+
+export async function handleSignOut() {
+   await signOut(()=>{
+        router.push('/')
+    })
+    
 }
 
 export default NavBar;
--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,12 +1,17 @@
 import Link from 'next/link'
+import {useRouter} from 'next/router'
+import {useSession} from 'next-auth/react'
 
 function Subscribe() {
+    const router = useRouter()
+    const {data: session} = useSession()
     return(
         <div className='bg-gradient-to-bl from-indigo-100 to-indigo-400 h-full align-middle'>
           <div className='flex flex-col mx-auto w-6/12 text-center'>
             <p className='mx-auto mt-16 mb-10'>You have to subscribe to use this service</p>
-            <Link href='#'>
-              <a className='bg-lime-300 w-full lg:w-3/12 px-2 py-1 rounded-md h-10 mx-auto text-xl hover:bg-lime-600 flex align-middle justify-center'>Subscribe</a>
+            <button className='bg-lime-300 w-full lg:w-3/12 cursor-pointer px-2 py-1 rounded-md h-10 mx-auto text-xl hover:bg-lime-600 flex align-middle justify-center'>Subscribe</button>
+            <Link href='/'>
+                <a className='cursor-pointer underline my-4 hover:text-blue-500'>{session ? '' : 'Back to Home Page'}</a>
             </Link>
           </div>
         </div>
