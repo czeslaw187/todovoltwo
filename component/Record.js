f83c02284d76edbabea1204c5baacbcd2ba1eba8
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-02-09 21:04:06 +0000

    improve Record style

--- component/Record.js
+++ component/Record.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 function Record({record, rd, session}) {
     const [del,setDel] = useState(0)
+    const [visibility,setVisibility] = useState(true)
     useEffect(()=>{
         if (del) {
             rd.removeData(record.todo_id, session.session.user.email)
@@ -13,13 +14,14 @@ function Record({record, rd, session}) {
         <div className='flex flex-row align-middle justify-between bg-lime-100 rounded-md mx-auto w-full h-14 mb-1'>
             <div className={checkStyle.round}>
                 <input type="checkbox" id={record.todo_id} checked={record.isActive} onChange={()=>{rd.changeAct(record.todo_id, !record.isActive, session.session.user.email)}}/>
-                <label className='mr-4 ml-1' htmlFor={record.todo_id}></label>    
-                <p className={!record.isActive ? todoStyle.paragh : todoStyle.disabled}>{record.content}</p> 
+                <label htmlFor={record.todo_id}></label>    
+                
 
                                
             </div>            
-            <div className='opacity-0 hover:opacity-100 w-full flex justify-end'>
-                <button className='text-gray-400 ml-auto text-xl mr-5' onClick={()=>{setDel(true)}}>X</button>
+            <div className='w-full flex justify-between' onMouseEnter={()=>setVisibility(false)} onMouseLeave={()=>setVisibility(true)}>
+                <p className={`w-full mr-auto my-auto ml-4 ${record.isActive && 'text-slate-300 line-through'}`}>{record.content}</p> 
+                <button className='text-gray-400 ml-auto text-xl mr-5' hidden={visibility} onClick={()=>{setDel(true)}}>X</button>
             </div>  
         </div>
      );
